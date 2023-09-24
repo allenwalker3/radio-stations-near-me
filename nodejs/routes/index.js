26fa5b74ce72a67a25cf3e3ac7a5d7bff46b5ac1
Allen Walker <allenwalker@Allens-Mac-mini.local>
2023-09-24 17:33:35 -0600

    validate GET params

--- nodejs/routes/index.js
+++ nodejs/routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 const { Sequelize, QueryTypes } = require('sequelize');
 const { Station } = require('../models');
+const { Formats } = require('../constants/constants')
 const fs = require('fs');
 
 // db init
@@ -22,14 +23,14 @@ const queryInterface = sequelize.getQueryInterface();
 })
 
 /* GET home page. */
-router.get('/', async (req, res, next) => {
-  //{ distance: '100', am: 'true', fm: 'false', loc_x :number, loc_y :number}
-  //sequelize.query(`SELECT ST_Distance_Sphere(ST_GeomFromText('POINT(${req.query.loc_y} ${req.query.loc_x})'), geom, 'miles') as distance
+router.get('/', async (req, res) => {
   let results;
   const services = [];
-  if (req.query.loc_x && req.query.loc_y) {
+  let { loc_x, loc_y, distance } = req.query;
+  distance = Number(distance);
+  if (Number(loc_x) && Number(loc_y) && distance && distance > 0 && distance <= 200) {
     results = await sequelize.query(`
-      SELECT (ST_Distance_Sphere(ST_GeomFromText('POINT(${req.query.loc_y} ${req.query.loc_x})'), geom) / 1609.34) as distance,
+      SELECT (ST_Distance_Sphere(ST_GeomFromText('POINT(${loc_y} ${loc_x})'), geom) / 1609.34) as distance,
           id,
           callSign, 
           frequency, 
@@ -50,15 +51,12 @@ router.get('/', async (req, res, next) => {
         Stations
       WHERE
           ST_Distance_Sphere(
-          POINT(${req.query.loc_y}, ${req.query.loc_x}),
+          POINT(${loc_y}, ${loc_x}),
           geom
-          ) < 1609.34 * ${req.query.distance}`, { type: QueryTypes.SELECT });
-
-    //   console.log(results);
+          ) < 1609.34 * ${distance}`, { type: QueryTypes.SELECT });
   }
 
   res.send(results);
-  //res.render('index', { title: 'Express' });
 });
 
 router.post('/', async (req, res) => {
@@ -66,13 +64,13 @@ router.post('/', async (req, res) => {
   const callSign = req.body.callSign;
   const format = req.body.format;
   const service = req.body.service;
-  //await Report.create({callSign: req.body.callSign, format: req.body.format}).catch(err => console.log(err))
-  const stations = await Station.findAll({ where: { callSign: callSign, service: service } });
-  for (const station of stations) {
-    station.format = format
-    station.save()
+  if (Formats.includes(format) && callSign?.length < 8 && (service === 'FM' || service === 'AM') ) {
+    const stations = await Station.findAll({ where: { callSign: callSign, service: service } });
+    for (const station of stations) {
+      station.format = format
+      station.save()
+    }
   }
-
   const date = new Date().getDate(); //Current Date
   const month = new Date().getMonth() + 1; //Current Month
   const year = new Date().getFullYear(); //Current Year
@@ -84,7 +82,7 @@ router.post('/', async (req, res) => {
 
   const log = `${ipAddress} ${dateTime} : ${callSign} : ${format}\n`
 
-  
+
   fs.writeFile('./logs/updates.txt', log, { flag: 'a+' }, (err) => {
     if (err) {
       throw err;
